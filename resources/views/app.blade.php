f99f08e0ed1dffe2890b61f1c0dee47f8b10c283
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2021-05-31 17:38:27 +0300

    update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,22 +4,29 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title','A default title')</title>
-        <meta name="keywords" content="@yield('meta_keywords','some default keywords')">
-        <meta name="description" content="@yield('meta_description','default description')">
+        <title>@yield('title','')</title>
+        <meta name="keywords" content="@yield('meta_keywords','')">
+        <meta name="description" content="@yield('meta_description','')">
         <link rel="canonical" href="{{url()->current()}}"/>
 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
     <body>
-    <header class="header">
-        <div class="container">
-            HEADER
-        </div>
-    </header>
-        <div class="container">
-            @yield('content')
-        </div>
+        <header class="header">
+            <div class="container">
+                HEADER
+            </div>
+        </header>
+        <main class="main">
+            <div class="container">
+                @yield('content')
+            </div>
+        </main>
+        <footer class="footer">
+            <div class="container">
+                FOOTER
+            </div>
+        </footer>
     </body>
 </html>
