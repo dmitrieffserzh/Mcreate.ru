9cac8206eff38fad22f6cc3611db7dab3bf6659f
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2021-10-22 18:34:37 +0300

    update

---
+++ resources/views/pages/page-development.blade.php
@@ -0,0 +1,47 @@
+@extends('app')
+
+@section('title', $page['meta'][0]['title'])
+@section('meta_keywords', $page['meta'][0]['keywords'])
+@section('meta_description', $page['meta'][0]['description'])
+
+@section('content')
+    <section class="section section--page section-bg--page-{{ end($segment) }}">
+        <div class="container">
+            <div class="page-section">
+                <h1 class="page-section__title">{{ $page['title'] }}</h1>
+                <p class="page-section__description">{!! strip_tags($page['content']) !!}</p>
+            </div>
+        </div>
+    </section>
+
+    <div id="map" style="width:100%; height:60vh;"></div>
+    @push('header-scripts')
+        <script src="//api-maps.yandex.ru/2.0/?load=package.standard,package.geoObjects&lang=ru-RU" type="text/javascript"></script>
+    @endpush
+    @push('footer-scripts')
+        <script>
+            ymaps.ready(init);
+            function init() {
+                var myMap = new ymaps.Map("map", {
+                        center: [55.751979, 37.617499],
+                        zoom: 15
+                    }),
+                    myPlacemark1 = new ymaps.Placemark([55.751979, 37.617499], {
+                        hintContent: 'Надпись, которая всплаывет при наведении на метку'
+                    }, {
+                        iconImageHref: 'https://static.tildacdn.com/tild3061-3235-4537-b066-616662373363/Group_783.svg',
+                        iconImageSize: [130, 130],
+                        iconImageOffset: [-65, -110]
+                    })
+
+                myMap.geoObjects
+                    .add(myPlacemark1)
+            }
+        </script>
+    @endpush
+    <style>
+        .ymaps-layers-pane {
+            -webkit-filter: grayscale(100%);
+        }
+    </style>
+@endsection
