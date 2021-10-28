5574544df4cbd191a0f203046d898f0eecaadc73
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2021-10-28 16:31:51 +0300

    update

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -12,8 +12,11 @@ class WorkController extends Controller
     }
 
 	public function show ($slug) {
-		$work = Work::where( 'slug', '=', $slug )->get()->load( 'meta' )->toArray();
+		$work = Work::where( 'slug', '=', $slug )->firstOrFail()->load( 'meta' )->toArray();
 		$works = Work::all()->toArray();
+
+		$work['work']    = json_decode( $work['work'], true );
+		$work['results'] = json_decode( $work['results'], true );
 
 		return view( 'pages.page-work-show', [
 			'work' => $work,
--- resources/views/pages/page-work-show.blade.php
+++ resources/views/pages/page-work-show.blade.php
@@ -1,19 +1,19 @@
 @extends('app')
 
-@section('title', $work[0]["meta"][0]['title'])
-@section('meta_keywords',$work[0]["meta"][0]['keywords'])
-@section('meta_description', $work[0]["meta"][0]['description'])
+@section('title', $work["meta"][0]['title'])
+@section('meta_keywords',$work["meta"][0]['keywords'])
+@section('meta_description', $work["meta"][0]['description'])
 
 @section('content')
-    <section class="section section--page section-bg--page-" @if($work[0]['img_main']) style="
-            background: url('{{ $work[0]['img_main'] }}');
+    <section class="section section--page section-bg--page-" @if($work['img_main']) style="
+            background: url('{{ $work['img_main'] }}');
             background-repeat: no-repeat;
             background-position: center center;
             background-size: cover;
             "@endif>
         <div class="container">
             <div class="page-section">
-                <h1 class="page-section__title">{!! $work[0]['title'] !!}</h1>
+                <h1 class="page-section__title">{!! $work['title'] !!}</h1>
                 <p class="page-section__description">test desc</p>
             </div>
         </div>
@@ -21,29 +21,45 @@
     <section class="section">
         <div class="container">
             <div class="works-content">
-                {{ strip_tags($work[0]['content']) }}
+                {{ strip_tags($work['content']) }}
             </div>
         </div>
     </section>
     <section class="section">
         <div class="container">
             <h2 class="section__title">Что мы сделали?</h2>
+            <div class="work">
+                @foreach($work['work'] as $item)
+                    <div class="work-item">
+                        <div class="work-item__number"></div>
+                        <h3 class="work-item__title">{{ $item['Описание'] }}</h3>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
     <section class="section">
         <div class="container">
             <h2 class="section__title">Результаты</h2>
+            <div class="works-results">
+                @foreach($work['results'] as $item)
+                    <div class="works-results-item">
+                        <div class="works-results-item__number">{{ $item['Цифра'] }}.</div>
+                        <div class="works-results-item__desc">{{ $item['Описание'] }}</div>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
     <section class="section section--works">
         <div class="works">
-        @foreach($works as $item)
+            @foreach($works as $item)
                 <div class="works-item">
                     <a class="works-item__link" href="{{ route('works.show', ['slug' => $item['slug']]) }}"></a>
                     <img class="works-item__img" src="{{ $item['img_cover'] }}" alt="">
                     <h3 class="works-item__title">{{ $item['title'] }}</h3>
                 </div>
-        @endforeach
+            @endforeach
         </div>
     </section>
 @endsection
